82a32ab25536fcb09b57e709be428fc03542d865
shahenaz monia <shahenaz.monia@igenius.ai>
2026-01-11 11:36:58 +0100

    fix Grid

--- src/modules/Products/views/index.tsx
+++ src/modules/Products/views/index.tsx
@@ -23,58 +23,61 @@ export const Products = () => {
   }
 
   return (
-    <Grid>
+    <>
       <Title>Products with Discount Higher Than 10%</Title>
-      {productsWithDiscountHigherThan10.map((product) => {
-        return (
-          <Grid.Col span={4} key={product.id}>
-            <Card shadow="sm" padding="lg" radius="md" withBorder>
-              <Card.Section>
-                <Image src={product.image} height={160} alt={product.name} />
-              </Card.Section>
-              {product.isAvailable && <Pill>Available</Pill>}
-              <Group justify="space-between" mt="md" mb="xs">
-                <Text fw={500}>{product.name}</Text>
-                <Badge color="pink">On Sale</Badge>
-              </Group>
+      <Grid>
+        {productsWithDiscountHigherThan10.map((product) => {
+          return (
+            <Grid.Col span={4} key={product.id}>
+              <Card shadow="sm" padding="lg" radius="md" withBorder>
+                <Card.Section>
+                  <Image src={product.image} height={160} alt={product.name} />
+                </Card.Section>
+                {product.isAvailable && <Pill>Available</Pill>}
+                <Group justify="space-between" mt="md" mb="xs">
+                  <Text fw={500}>{product.name}</Text>
+                  <Badge color="pink">On Sale</Badge>
+                </Group>
 
-              <Text size="sm" c="dimmed">
-                {product.description}
-              </Text>
+                <Text size="sm" c="dimmed">
+                  {product.description}
+                </Text>
 
-              <Button color="blue" fullWidth mt="md" radius="md">
-                Order Now
-              </Button>
-            </Card>
-          </Grid.Col>
-        );
-      })}
-
+                <Button color="blue" fullWidth mt="md" radius="md">
+                  Order Now
+                </Button>
+              </Card>
+            </Grid.Col>
+          );
+        })}
+      </Grid>
       <Title>Products with Discount Lower Than 10%</Title>
-      {productsWithDiscountLowerThan10.map((product) => {
-        return (
-          <Grid.Col span={4} key={product.id}>
-            <Card shadow="sm" padding="lg" radius="md" withBorder>
-              <Card.Section>
-                <Image src={product.image} height={160} alt={product.name} />
-              </Card.Section>
-              {product.isAvailable && <Pill>Available</Pill>}
-              <Group justify="space-between" mt="md" mb="xs">
-                <Text fw={500}>{product.name}</Text>
-                <Badge color="pink">On Sale</Badge>
-              </Group>
+      <Grid>
+        {productsWithDiscountLowerThan10.map((product) => {
+          return (
+            <Grid.Col span={4} key={product.id}>
+              <Card shadow="sm" padding="lg" radius="md" withBorder>
+                <Card.Section>
+                  <Image src={product.image} height={160} alt={product.name} />
+                </Card.Section>
+                {product.isAvailable && <Pill>Available</Pill>}
+                <Group justify="space-between" mt="md" mb="xs">
+                  <Text fw={500}>{product.name}</Text>
+                  <Badge color="pink">On Sale</Badge>
+                </Group>
 
-              <Text size="sm" c="dimmed">
-                {product.description}
-              </Text>
+                <Text size="sm" c="dimmed">
+                  {product.description}
+                </Text>
 
-              <Button color="blue" fullWidth mt="md" radius="md">
-                Order Now
-              </Button>
-            </Card>
-          </Grid.Col>
-        );
-      })}
-    </Grid>
+                <Button color="blue" fullWidth mt="md" radius="md">
+                  Order Now
+                </Button>
+              </Card>
+            </Grid.Col>
+          );
+        })}
+      </Grid>
+    </>
   );
 };
