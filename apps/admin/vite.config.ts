237955f9b5a8def1445497c821309816d6816ffb
shahenaz monia <shahenaz.monia@igenius.ai>
2026-01-30 23:20:44 +0100

    add admin folder

---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
+  server: {
+    port: 4000,
+    cors: true,
+    origin: "http://localhost:3000",
+  },
+});
